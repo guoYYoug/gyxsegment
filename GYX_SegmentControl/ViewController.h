77c6434719b4cdad4b2b417b487f7f5506ab4a49
DLAB <dlab@dlabdemacbook-pro-2.local>
2016-07-01 16:58:42 +0800

    Initial Commit

---
+++ GYX_SegmentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GYX_SegmentControl
+//
+//  Created by DLAB on 16/7/1.
+//  Copyright © 2016年 DLAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GYX_SegmentControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GYX_SegmentControl
+//
+//  Created by DLAB on 16/7/1.
+//  Copyright © 2016年 DLAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
